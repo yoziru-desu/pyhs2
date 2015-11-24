7d0bdf1ada739427aafb73b33f17d4245517506d
yoziru-desu <ekinci.yasir@gmail.com>
2015-11-24 09:42:43 +0100

    Revert "Initial testing set-up"
    This reverts commit e2105fa369e8494478ce5ff194d36170361b5acd.

--- scripts/make_one_row.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-hive -e '
-set mapred.job.tracker=local;
-DROP TABLE IF EXISTS one_row;
-CREATE TABLE one_row (number_of_rows INT);
-INSERT OVERWRITE TABLE one_row SELECT COUNT(*) + 1 FROM one_row;
-'
--- scripts/make_test_database.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-hive -e '
-DROP DATABASE IF EXISTS test_database CASCADE;
-CREATE DATABASE test_database;
-CREATE TABLE test_database.dummy_table (a INT);
-'
